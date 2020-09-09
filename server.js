802e920dd632357259aff64df6c19b99de70f12b
rafeym <mohammadrafey0@gmail.com>
2020-09-09 08:57:46 -0400

    DEVELOP: api routes defined

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express')
+const connectDB = require('./config/db')
 
 const app = express()
 
-app.get('/', (req, res) => res.send('API Running'))
+// Connect Database
+connectDB()
+
+// Init MiddleWare
+app.use(express.json({ extended: false }))
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/posts', require('./routes/api/posts'))
 
 const PORT = process.env.PORT || 5000
 
