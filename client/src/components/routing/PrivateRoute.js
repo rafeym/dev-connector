980c33cfc8e5f4bd565b6d80c7faf58f7bcbd83b
rafeym <mohammadrafey0@gmail.com>
2020-09-16 13:48:53 -0400

    DEVELOP: Route protection complete

---
+++ client/src/components/routing/PrivateRoute.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Route, Redirect } from 'react-router-dom'
+
+// Redux
+import { connect } from 'react-redux'
+
+const PrivateRoute = ({
+  component: Component,
+  auth: { isAuthenticated, loading },
+  ...rest
+}) => (
+  <Route
+    {...rest}
+    render={(props) =>
+      !isAuthenticated && !loading ? (
+        <Redirect to='/login' />
+      ) : (
+        <Component {...props} />
+      )
+    }
+  />
+)
+
+PrivateRoute.propTypes = {
+  auth: PropTypes.object.isRequired,
+}
+
+const mapStateToProps = (state) => {
+  return {
+    auth: state.auth,
+  }
+}
+
+export default connect(mapStateToProps)(PrivateRoute)
