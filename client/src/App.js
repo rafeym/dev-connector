72be0231c73fbd0ea740ac03713bc02430342bc1
rafeym <mohammadrafey0@gmail.com>
2020-09-20 14:29:00 -0400

    DEVELOP: Added routes for register, login, dashboard, and private routes for create and edit profile and add education and experience

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,69 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import './App.css'
 import Navbar from './components/layout/Navbar'
 import Landing from './components/layout/Landing'
 import Login from './components/auth/Login'
 import Register from './components/auth/Register'
-
+import Dashboard from './components/dashboard/Dashboard'
+import Alert from './components/layout/Alert'
+import PrivateRoute from './components/routing/PrivateRoute'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import CreateProfile from './components/profile-form/CreateProfile'
+import EditProfile from './components/profile-form/EditProfile'
+import AddExperience from './components/profile-form/AddExperience'
+import AddEducation from './components/profile-form/AddEducation'
 
-import './App.css'
+// Redux
+import { Provider } from 'react-redux'
+import store from './store'
+import { loadUser } from './actions/auth'
+import setAuthToken from './utils/setAuthToken'
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token)
+}
 
 function App() {
+  useEffect(() => {
+    store.dispatch(loadUser())
+  }, [])
   return (
-    <Router>
-      <>
-        <Navbar />
-        <Route exact path='/' component={Landing} />
-        <section className='container'>
-          <Switch>
-            <Route path='/register' component={Register} />
-            <Route path='/login' component={Login} />
-          </Switch>
-        </section>
-      </>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <>
+          <Navbar />
+          <Route exact path='/' component={Landing} />
+          <section className='container'>
+            <Alert />
+            <Switch>
+              <Route exact path='/register' component={Register} />
+              <Route exact path='/login' component={Login} />
+              <PrivateRoute exact path='/dashboard' component={Dashboard} />
+              <PrivateRoute
+                exact
+                path='/create-profile'
+                component={CreateProfile}
+              />
+              <PrivateRoute
+                exact
+                path='/edit-profile'
+                component={EditProfile}
+              />
+              <PrivateRoute
+                exact
+                path='/add-experience'
+                component={AddExperience}
+              />
+              <PrivateRoute
+                exact
+                path='/add-education'
+                component={AddEducation}
+              />
+            </Switch>
+          </section>
+        </>
+      </Router>
+    </Provider>
   )
 }
 
