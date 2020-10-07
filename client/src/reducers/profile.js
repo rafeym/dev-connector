78f0ed97357b0d376338ff174d3dda81d1be44da
rafeym <mohammadrafey0@gmail.com>
2020-10-07 17:42:17 -0400

    DEVELOP: reducer and action created to get all profiles, get profile by id and get github repos

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -3,6 +3,8 @@ import {
   PROFILE_ERROR,
   CLEAR_PROFILE,
   UPDATE_PROFILE,
+  GET_PROFILES,
+  GET_REPOS,
 } from '../actions/types'
 
 const initialState = {
@@ -20,6 +22,18 @@ export default function (state = initialState, action) {
       return {
         ...state,
         profile: action.payload,
+        loading: false,
+      }
+    case GET_PROFILES:
+      return {
+        ...state,
+        profiles: action.payload,
+        loading: false,
+      }
+    case GET_REPOS:
+      return {
+        ...state,
+        repos: action.payload,
         loading: false,
       }
     case PROFILE_ERROR:
